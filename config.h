e0931afaa3dd758f0c599103190b0132e197f348
rtfmexe <rtfmexe@users.noreply.github.com>
2021-01-27 20:16:28 -0300

    packettracer float windows

--- config.h
+++ config.h
@@ -29,8 +29,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Steam",     NULL,       NULL,       0,            1,           -1 },
+	{ "Steam",     NULL,       NULL,      0,           1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    { "PacketTracer7",  NULL,   NULL,     0,             1,   -1 },
 };
 
 /* layout(s) */
